371ad9764dc3bed887d166231f7d600f286a5aba
mohd <amir12.java@gmail.com>
2025-09-14 21:44:14 +0530

    updated

--- src/main/java/com/example/spring/ai/OllamaApiController.java
+++ src/main/java/com/example/spring/ai/OllamaApiController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/ollama")
 @RequiredArgsConstructor
 public class OllamaApiController {
 
